7db9669ee7379900c8299805a222f88bf7725f58
Jimmy Arts <arts.jimmy@gmail.com>
2016-11-14 22:27:35 +0100

    Added local reference to SwiftServerExample-Common for debugging purposes

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+        .Package(url: "../SwiftServerExample-Common", versions: Version(0,0,1)..<Version(1,0,0))
     ]
 )
